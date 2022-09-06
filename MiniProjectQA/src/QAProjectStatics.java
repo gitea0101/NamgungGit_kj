de968baf189a8a2671b5647c485243a8617da9f3
gitea0101 <skarndrlxo@naver.com>
2022-09-06 14:20:22 +0900

    aa

--- MiniProjectQA/src/QAProjectStatics.java
+++ MiniProjectQA/src/QAProjectStatics.java
@@ -4,29 +4,30 @@ public class QAProjectStatics {
     public int StaticsFunction(Statement stmt){
         System.out.println("통계");
        
-        int printAllStastics() {
-            String QUERY = "select * from QAtable";
-    
-            return 1;
-    
-        }
-    
-        int sumAnsFunction(){
-                String QUERY = "select * from QuestionN";
-    
-                return 1;
-            }
-    
         
-    
-        int mostSelectFunction(){
-    
-            return 1;
-        }
-    
-        int sumUserFunction(){
-            return 1;
-        }
        return 1;
+    }
+    public int printAllStastics() {
+        String QUERY = "select * from QAtable";
+
+        return 1;
+
+    }
+
+    public int sumAnsFunction(){
+            String QUERY = "select * from QuestionN";
+
+            return 1;
+        }
+
+    
+
+        public int mostSelectFunction(){
+
+        return 1;
+    }
+
+    public int sumUserFunction(){
+        return 1;
     }
 }
